4521e77a6a1e73bc6c32687150acf41907cb2d49
siro4ik <deputatov0@mail.ru>
2025-03-04 15:40:10 +0300

    реализовал счетчик, начал делать сетку перетянул цветок из контейнера

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 // Нужно сделать счетчик товаров над корзиной, который будет динамически меняться при добавлении товара в корзину
 // добавить расширение области комментариев при нажатии на -> +
 // сделать анимацию сменяемости товаров по таймеру или при нажатии ручками (в макете товары показаны разом, нужно сделать акцент на определенном)
+
+
+
+
+document.getElementById('changeNumberButton').addEventListener('click', function() {
+  let textElement = document.getElementById('number');
+  let currentNumber = parseInt(textElement.textContent);
+  let newNumber = currentNumber + 1;
+  textElement.textContent = newNumber;
+});
